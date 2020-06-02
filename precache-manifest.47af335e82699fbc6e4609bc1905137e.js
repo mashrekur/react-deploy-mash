b27665a62c6bf7a8c8a2a353f352bb3de129a154
Mashrekur Rahman <mrahman18@crimson.ua.edu>
2020-06-02 01:18:53 -0500

    Updates

---
+++ precache-manifest.47af335e82699fbc6e4609bc1905137e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e2ca665382ca56bf7521d90ba6c666",
+    "url": "/react-deploy-mash/index.html"
+  },
+  {
+    "revision": "8375f38d95a08c199ce1",
+    "url": "/react-deploy-mash/static/css/main.ae7ef5a2.chunk.css"
+  },
+  {
+    "revision": "c623d30628c24fefe589",
+    "url": "/react-deploy-mash/static/js/2.ca0397b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-mash/static/js/2.ca0397b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8375f38d95a08c199ce1",
+    "url": "/react-deploy-mash/static/js/main.3e7a478f.chunk.js"
+  },
+  {
+    "revision": "b97dded61bc25593346f",
+    "url": "/react-deploy-mash/static/js/runtime-main.6340f0cd.js"
+  },
+  {
+    "revision": "0b1239c0362f4dc3a7d96b2a13c47087",
+    "url": "/react-deploy-mash/static/media/logo.0b1239c0.svg"
+  },
+  {
+    "revision": "5c90a1b7f6d9db4b2dccfc8ad3058776",
+    "url": "/react-deploy-mash/static/media/profile_image.5c90a1b7.png"
+  }
+]);
